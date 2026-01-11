4c269bb583be02f568a91e0e27a147eb9a787497
basesnel <basesnel@gmail.com>
2026-01-11 14:25:07 +0200

    Comment for task 5-generic is uploaded.

--- src/generic/5.ts
+++ src/generic/5.ts
@@ -1,6 +1,11 @@
 /*
   Вам потрібно реалізувати інтерфейс KeyValuePair, який описує пару ключ-значення.
   Використовуйте generics, щоб цей інтерфейс міг працювати з будь-якими типами ключів та значень.
+
+  interface KeyValuePair {
+    key;
+    value;
+  }
 */
 
 interface KeyValuePair<Tkey, Tvalue> {
