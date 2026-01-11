a87b6cad048e9da33507e73eeee988370e788f92
basesnel <basesnel@gmail.com>
2026-01-11 14:22:49 +0200

    Comment for task 6-generic is uploaded.

--- dist/generic/6.js
+++ dist/generic/6.js
@@ -5,6 +5,19 @@
   залишивши ім'я та прізвище без змін.
 
   Виправте тип у аргументі функції так, щоб не було помилок типу.
+
+  type User = {
+    name: string;
+    surname: string;
+    email: string;
+    password: string;
+  }
+
+  function createOrUpdateUser(initialValues: User) {
+    // Оновлення користувача
+  }
+
+  createOrUpdateUser({ email: 'user@mail.com', password: 'password123' });
 */
 function createOrUpdateUser(initialValues) {
     // Оновлення користувача
