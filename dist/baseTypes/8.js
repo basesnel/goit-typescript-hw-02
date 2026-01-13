819b5376d278a4c2bfaa6013e7b282429480d3ce
basesnel <basesnel@gmail.com>
2026-01-13 19:43:00 +0200

    Comment for task 8-baseTypes is uploaded.

--- dist/baseTypes/8.js
+++ dist/baseTypes/8.js
@@ -1,5 +1,9 @@
-// Створіть тип "Gender", використовуючи union type, який може містити значення "male", "female".
-// Створіть змінну myGender цього типу.
+/*
+  Створіть тип "Gender", використовуючи union type, який може містити значення "male", "female".
+  Створіть змінну myGender цього типу.
+
+  const myGender;
+*/
 const myGender = 'male';
 export {};
 //# sourceMappingURL=8.js.map
--- src/baseTypes/8.ts
+++ src/baseTypes/8.ts
@@ -1,5 +1,9 @@
-// Створіть тип "Gender", використовуючи union type, який може містити значення "male", "female".
-// Створіть змінну myGender цього типу.
+/*
+  Створіть тип "Gender", використовуючи union type, який може містити значення "male", "female".
+  Створіть змінну myGender цього типу.
+
+  const myGender;
+*/
 
 type Gender = 'male' | 'female';
 
