8602abee2500e1f3c8e73898920d4672f5cafd05
basesnel <basesnel@gmail.com>
2026-01-13 19:31:11 +0200

    Comment for task 1-generic is uploaded.

--- src/generic/1.ts
+++ src/generic/1.ts
@@ -1,6 +1,17 @@
 /*
   Є функція getPromise(), яка повертає проміс, що дозволяється в масив, що містить рядки та числа.
   Доповніть цю функцію, використовуючи generics, щоб вона повертала правильний тип.
+
+  function getPromise () {
+    return new Promise((resolve) => {
+      resolve(['Text', 50]);
+    });
+  }
+
+  getPromise()
+  .then((data) => {
+    console.log(data);
+  });
 */
 
 function getPromise(): Promise<Array<string | number>> {
@@ -8,9 +19,5 @@ function getPromise(): Promise<Array<string | number>> {
     resolve(['Text', 50]);
   });
 }
-
-// getPromise().then(data => {
-//   console.log(data);
-// });
 
 export { getPromise };
