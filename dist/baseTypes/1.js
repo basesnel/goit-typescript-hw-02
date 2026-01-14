c6e7ce42aaeedb7e46c653061a740e724922c746
basesnel <basesnel@gmail.com>
2026-01-14 20:17:50 +0200

    Comment for task 1-baseTypes is uploaded.

--- dist/baseTypes/1.js
+++ dist/baseTypes/1.js
@@ -1,4 +1,15 @@
-// Перетворіть цей код на TypeScript, вказавши відповідні типи для всіх змінних.
+/*
+Є наступний JavaScript код:
+
+  let age = 50;
+  let name = 'Max';
+  let toggle = true;
+  let empty = null;
+  let notInitialize;
+  let callback = (a) => { return 100 + a };
+
+Перетворіть цей код на TypeScript, вказавши відповідні типи для всіх змінних.
+*/
 let age = 50;
 let name = 'Max';
 let toggle = true;
